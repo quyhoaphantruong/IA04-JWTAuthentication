216a79279ad4e7a046ea500510d68cb9c79433e5
Phan Trương Quý Hòa <93481453+quyhoaphantruong@users.noreply.github.com>
2024-11-11 22:58:00 +0700

    cors handling

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require("cors");
 
 const app = express();
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) =>  {
